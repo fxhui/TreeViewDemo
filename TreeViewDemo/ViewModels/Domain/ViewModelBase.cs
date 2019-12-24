0cfee770461f6aa8521bf9fc265d3ad33e37d6d7
fxhui <m15757906377@163.com>
2019-12-24 20:53:54 +0800

    TreeViewDemo

---
+++ TreeViewDemo/ViewModels/Domain/ViewModelBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewDemo.ViewModels.Domain
+{
+    public static class NotifyPropertyChangedExtension
+    {
+        public static void MutateVerbose<TField>(this INotifyPropertyChanged instance, ref TField field, TField newValue, Action<PropertyChangedEventArgs> raise, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<TField>.Default.Equals(field, newValue)) return;
+            field = newValue;
+            raise?.Invoke(new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        protected ViewModelBase()
+        {
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected Action<PropertyChangedEventArgs> RaisePropertyChanged()
+        {
+            return args => PropertyChanged?.Invoke(this, args);
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TreeViewDemo/ViewModels/TreeViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TreeViewDemo.ViewModels.Domain;
+
+namespace TreeViewDemo.ViewModels
+{
+    public class TreeViewModel : ViewModelBase
+    {
+        private TypeModel selectItem;
+
+        public TypeModel SelectItem
+        {
+            get { return selectItem; }
+            set { this.MutateVerbose(ref selectItem, value, RaisePropertyChanged()); }
+        }
+
+        public ObservableCollection<TypeTreeModel> TypeList { get; set; } = new ObservableCollection<TypeTreeModel>();
+        public TreeViewModel()
+        {
+            TypeList = new ObservableCollection<TypeTreeModel>(GetData());
+        }
+
+        private List<TypeTreeModel> GetData()
+        {
+            List<TypeTreeModel> typeTrees = new List<TypeTreeModel>()
+            {
+                new TypeTreeModel()
+                {
+                    Id = 1,
+                    Name = "手机",
+                    ChildList = new ObservableCollection<TypeTreeModel>()
+                    {
+                        new TypeTreeModel(){ Id=2,Name="苹果" },
+                        new TypeTreeModel(){ Id=2,Name="华为",
+                            ChildList = new ObservableCollection<TypeTreeModel>()
+                            {
+                                new TypeTreeModel(){Id=5,Name="荣耀" }
+                            }},
+                        new TypeTreeModel(){ Id=2,Name="小米",
+                            ChildList = new ObservableCollection<TypeTreeModel>()
+                            {
+                                new TypeTreeModel(){Id=5,Name="红米" }
+                            }}
+                    }
+                },
+                new TypeTreeModel()
+                {
+                    Id=3,
+                    Name="笔记本",
+                    ChildList = new ObservableCollection<TypeTreeModel>()
+                    {
+                        new TypeTreeModel(){Id=4,Name="联想"}
+                    }
+                },
+                new TypeTreeModel()
+                {
+                    Id=3,
+                    Name="耳机"
+                }
+            };
+            return typeTrees;
+        }
+        public ICommand SelectItemChangeCommand
+        {
+            get
+            {
+                return new CommandBase((param) =>
+                {
+                    if (param != null)
+                        SelectItem = (TypeModel)param;
+                });
+            }
+        }
+    }
+
+    public class TypeTreeModel : TypeModel
+    {
+        public ObservableCollection<TypeTreeModel> ChildList { get; set; } = new ObservableCollection<TypeTreeModel>();
+    }
+    public class TypeModel : ViewModelBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
